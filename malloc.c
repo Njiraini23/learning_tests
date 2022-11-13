7f859986a9677f3f9c125d845a1ee4cb29b2d3cf
Njiraini23 <njiraini88@gmail.com>
2022-11-13 08:19:22 +0000

    Malloc practices

--- malloc.c
+++ malloc.c
@@ -3,9 +3,16 @@
 
 int main(void)
 {
-	char = 'a';
-	char *p1;
-		
-	printf("%lu\n", sizeof(malloc(sizeof(p1) *5)));
+	int n;
+	char *ar;
+
+	n = 5;
+	ar = malloc(n * sizeof(char));
+	ar[0] = 'c';
+	ar[1] = 'o';
+	ar[2] = 'o';
+	ar[3] = 'l';
+	ar[4] = '\0';
+	printf("%s\n", ar);
 	return (0);
 }
