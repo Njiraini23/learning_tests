effc4d11ae319b870f0185ad2070dde942f97a20
Njiraini23 <njiraini88@gmail.com>
2022-11-25 15:28:44 +0000

    nodes practice

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
+
 
 int main(void)
 {
-	hi();
-	hello();
-	habari();
-	wagwan();
+	node *head;
+	node one;
+	node two;
 
+	one.data = 4;
+	two.data = 6;
+
+	head = &one;
+	one.next = &two;
+
+	printf("%d\n", head->data);
+	printf("%d\n",head->next->data);
 	return 0;
 }
