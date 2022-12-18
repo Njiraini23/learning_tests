8339f3572deaa8b4c1d49ce2c00c00edfda03421
Njiraini23 <njiraini88@gmail.com>
2022-12-18 19:21:03 +0000

    fork

--- project_tests/fork.c
+++ project_tests/fork.c
@@ -8,18 +8,18 @@ int main(void)
 	int wstatus;
 	pid_t pid;
 
-	printf("Before fprk\n")
-		pid = fork();
+	printf("Before fork\n");
+	pid = fork();
 	if (pid == 0)
 	{
-		printf("I amchild process with ID = %d\n", getpid());
+		printf("I am hild process with ID = %d\n", getpid());
 		printf("I have a parent with ID = %d\n", getppid());
 	} else
 	{
 		wait(&wstatus);
 		printf("%d\n", WIFEXITED(wstatus));
-		printf("I am parent process with ID = %d\n" getpid());
-		printf(I have a child with ID = %d\n\n", pid);
+		printf("I am parent process with ID = %d\n", getpid());
+		printf("I have a child with ID = %d\n\n", pid);
 	}
 	return (0);
 }
