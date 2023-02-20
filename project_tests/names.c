7a1a51ef8eb6b503b23834b16162d2470adcf281
Njiraini23 <njiraini88@gmail.com>
2023-02-20 15:07:17 +0000

    strngs.h

---
+++ project_tests/names.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	string names[] = {"Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"};
+
+	for (str i = 0; i < 7; i++) 
+	{ 
+		if (strcmp(names[i], "Ron") == 0)
+		{
+			printf("Found\n");
+			return 0;
+		}
+	}
+	printf("Not found\n");
+	return 1;
+}
+	
