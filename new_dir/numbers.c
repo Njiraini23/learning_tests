0ca8965d643126842a7e5b6a26840fd7b6143a25
Njiraini23 <njiraini88@gmail.com>
2023-02-27 20:03:24 +0000

    bash tests

---
+++ new_dir/numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	int i;
+	int j;
+
+	for ((i = 0; i <= 100; i++) && j = 0; j <= 10; j++)
+
+		printf("%d\n" i, j);
+
+	return (0);
+}
---
+++ new_dir/test3.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash 
+
+ARR=(1 2 3)
+
+echo "${ARR[2]}"
