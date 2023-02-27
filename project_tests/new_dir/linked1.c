e7a450d8eb3d972e9b7dee452d448d9c7a607368
Njiraini23 <njiraini88@gmail.com>
2023-02-27 11:28:18 +0000

    love.c

---
+++ project_tests/new_dir/linked1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+
+#include <stdio.h>
+
+int main(void)
+
+Struct Node
+{
+	int data;
+	Node *next;
+} 
+Node* A;
+A = NULL; // creates an empt list
+
+Node *tmp = (Node*)malloc(sizeof (Node)  
+temp->data = 2; 
+temp->next = NULL;
+
+A = temp;
